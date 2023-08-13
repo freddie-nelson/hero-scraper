9537f5d8684fcbdb6e50ee5fd506aecfe51132fd
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2023-08-13 19:04:32 +0100

    chore: bump version

--- src/utils/userAgents.ts
+++ src/utils/userAgents.ts
@@ -1,2 +1,2 @@
-export const windows: string = "~ chrome >= 105 && windows >= 10";
-export const mac: string = "~ chrome >= 105 && mac >= 10.15";
+export const windows = "~ chrome >= 105 && windows >= 10";
+export const mac = "~ chrome >= 105 && mac >= 10.15";
